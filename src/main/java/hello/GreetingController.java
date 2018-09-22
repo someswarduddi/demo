5fac62164d247eacc8ae68f1a541b98bd14995b3
Someswar Duddi <someswar.duddi@gmail.com>
2018-09-22 17:04:13 +0100

    second commit

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+// new change
 @RestController
 public class GreetingController {
 
